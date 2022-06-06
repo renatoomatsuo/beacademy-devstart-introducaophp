78a0617ba6433e9ef6eabdccab3629a76cdf3c2f
Renato Matsuo Bezerra Tanaka <101281804+renatoomatsuo@users.noreply.github.com>
2022-06-06 18:50:26 -0300

    Create variaveis.php

---
+++ Aula2/variaveis.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Renato";
+$idade = 30;
+$nomeGato = "Chico";
+$status = "noivo";
+$nomeNoiva = "Natália";
+$profissao = "programador";
+
+
+
+
+echo "Meu nome é" . $nome . ", eu tenho " . $idade . " anos." . " Sou " . $status . " com a ". $nomeNoiva . ". Eu sou " $profissao. ". E temos 1 gato que se chamam " . $nomeGato . ".";
