20d4ae97ab9fc87fa7f921f12ab2786f3e2d17f8
Renato Matsuo Bezerra Tanaka <101281804+renatoomatsuo@users.noreply.github.com>
2022-06-06 18:53:55 -0300

    Create concatenacao.php

---
+++ Aula3/concatenacao.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "Renato";
+$sobrenome = "Tanaka";
+$idade = 30;
+$cidade = "São Paulo";
+
+echo $nome . $sobrenome;
+echo "<br>";
+echo "Eu me chamo {$nome} {$sobrenome}, tenho {$idade} anos e moro em {$cidade}";
+
+//exemplo concatenado:
+
+echo "<br>";
+$nome2 = "Natália";
+$sobrenome2 = "Jornay";
+
+echo $nome2 . $sobrenome2;
