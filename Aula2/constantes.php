fab5f4639e3fcb021e771aa70517c9a62dbf105a
Renato Matsuo Bezerra Tanaka <101281804+renatoomatsuo@users.noreply.github.com>
2022-06-06 18:43:10 -0300

    Create constantes.php

---
+++ Aula2/constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+Forma antiga de constante:
+
+definir ('CPF' , '123.123.123-12');
+
+Forma mais atual de constantes:
+
+Por facilidade é comum o uso de constantes com letra toda em maiuscula
+
+const NOME_COMPLETO = "Renato Matsuo Bezerra Tanaka";
+
+echo "Meu nome é" . NOME_COMPLETO;
